f17f62c18793143ae4d931ed26e6963fc475fcee
phobeer <hamburgerisperfectfood@gmail.com>
2023-06-19 21:06:06 +0900

    irc

---
+++ irc/src/commands/impl/QuitCommand.cpp
@@ -0,0 +1,13 @@
+#include "commands/Command.hpp"
+
+QuitCommand::QuitCommand(Server *server, bool authRequired) : Command(server, authRequired) {}
+
+QuitCommand::~QuitCommand() {}
+
+void QuitCommand::execute(Client *client, std::vector<std::string> arguments) {
+
+	std::string reason = arguments.empty() ? "Leaving..." : arguments.at(0);
+	reason = reason.at(0) == ':' ? reason.substr(1) : reason;
+
+	client->write(RPL_QUIT(client->getPrefix(), reason));
+}
